ac8c14b318f9149e32c90b8d331a81f4e92c5a37
Stanislav Yalovnichiy <syalovnichiy@ya.ru>
2020-08-15 22:10:44 +0300

    Supported command line args

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+// CommandLineArgs - command line args structure
+type CommandLineArgs struct {
+	targetPath string
+	maxDepth   int
+}
+
+var parsedArgs CommandLineArgs
+
+func init() {
+	var targetPath string
+	var maxDepth int
+
+	flag.StringVar(&targetPath, "t", ".", "Entrypoint path")
+	flag.IntVar(&maxDepth, "max-depth", -1, "Max depth of recursion")
+
+	flag.Parse()
+
+	parsedArgs.targetPath = targetPath
+	parsedArgs.maxDepth = maxDepth
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(parsedArgs)
 }
