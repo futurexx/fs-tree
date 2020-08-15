e0ed5b92dcfb318c03b65e533bb89b4c649a60c4
Stanislav Yalovnichiy <syalovnichiy@ya.ru>
2020-08-15 23:43:07 +0300

    Added command line args checker

--- main.go
+++ main.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 // CommandLineArgs - command line args structure
 type CommandLineArgs struct {
 	targetPath string
-	maxDepth   int
+	maxDepth   uint
 }
 
 var parsedArgs CommandLineArgs
 
+// CheckArgs - checking command line arguments
+func CheckArgs() error {
+	fileStat, err := os.Stat(parsedArgs.targetPath)
+	if err != nil {
+		errorString := fmt.Sprintf("Failed to match path `%s`\n", parsedArgs.targetPath)
+		return errors.New(errorString)
+	}
+
+	if !fileStat.IsDir() {
+		return errors.New("Target must be a dir")
+	}
+
+	return nil
+}
+
 func init() {
 	var targetPath string
-	var maxDepth int
+	var maxDepth uint
 
 	flag.StringVar(&targetPath, "t", ".", "Entrypoint path")
-	flag.IntVar(&maxDepth, "max-depth", -1, "Max depth of recursion")
+	flag.UintVar(&maxDepth, "max-depth", 0, "Max depth of recursion (positive number)")
 
 	flag.Parse()
 
@@ -27,5 +45,11 @@ func init() {
 }
 
 func main() {
+	err := CheckArgs()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(parsedArgs)
 }
